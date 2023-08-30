933e0575155aea73922b5bb8f02c3257e0da0d0a
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2023-08-30 15:49:31 +0200

    Remove tinet-term from tinet-bridge.py because it changed too much xD Fix some IDE errors for tinet-bridge.py

--- tinet-bridge.py
+++ tinet-bridge.py
@@ -337,53 +337,6 @@ def main():
 
         sys.exit(0)
 
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(Fore.LIGHTBLACK_EX + f"Connecting to {SERVER_ADDRESS}:{SERVER_PORT} ...")
-        sock.connect((SERVER_ADDRESS, SERVER_PORT))
-        print(Fore.GREEN + f"Connected to {SERVER_ADDRESS}:{SERVER_PORT} !")
-
-        print(Fore.YELLOW + "Logging in..")
-        sock.send("SERIAL_CONNECTED".encode())
-        sock.recv(4096)
-
-        sock.send(f"LOGIN:{CALC_ID}:{USERNAME}:{TOKEN}".encode())
-        loggedIn = sock.recv(4096).decode().strip()
-
-        if loggedIn != "LOGIN_SUCCESS":
-            print(Fore.RED + "Login failed!")
-            print(Fore.RED + loggedIn)
-            sys.exit(1)
-        print(Fore.GREEN + "Logged in as", USERNAME)
-
-        print(Fore.CYAN + f"You are now connected to the socket server at {SERVER_ADDRESS}:{SERVER_PORT}.")
-        print("Type your commands, and press Enter to send.")
-        print("Type '?' to show a list of available commands.")
-
-        signal.signal(signal.SIGINT, sigint_handler)
-
-        while True:
-            user_input = input(Fore.YELLOW + ">>> ")
-
-            if not user_input.strip():
-                print(Fore.YELLOW + "Please enter a command.")
-                continue
-
-            if user_input.lower() == "exit":
-                break
-            elif user_input.lower() == "clear":
-                os.system("cls" if os.name == "nt" else "clear")
-            elif user_input == "?":
-                command_help()
-            else:
-                sock.send(user_input.encode())  # Encode the user input as bytes
-                receive_response(sock)
-
-    except KeyboardInterrupt:
-        print(Fore.RED + "CTRL-C received. Command cancelled.")
-    finally:
-        sock.close()
-        print(Fore.CYAN + "Socket connection closed.")
 
 if __name__ == "__main__":
     main()
